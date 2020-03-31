29b484e1112f2e1f9b3b2a7dc030511cd67a6cc5
wa-crypto <siag2746@gmail.com>
2020-03-31 14:51:12 +0000

    started working on 3/31

--- main.py
+++ main.py
@@ -1,145 +1,47 @@
-"""
-'''
-This code will take time to run, be PATIENT!!!!
-'''
-import matplotlib.pyplot as plt 
-import os.path  
-import PIL
-import PIL.ImageDraw            
+`from PIL import Image, ImageDraw, ImageFilter
 
-def round_corners_one_image(original_image, percent_of_side=.3):
- """
-""" Rounds the corner of a PIL.Image
-  
-  original_image must be a PIL.Image
-  Returns a new PIL.Image with rounded corners, where
-  0 < percent_of_side < 1
-  is the corner radius as a portion of the shorter dimension of original_image
-  """
-"""
-  #set the radius of the rounded corners
-  width, height = original_image.size
-  radius = int(percent_of_side * min(width, height)) # radius in pixels
-  
-  ###
-  #create a mask
-  ###
-  
-  
-  #start with transparent mask
-  rounded_mask = PIL.Image.new('RGBA', (width, height), (127,0,127,0))
-  drawing_layer = PIL.ImageDraw.Draw(rounded_mask)
-  
-  # Overwrite the RGBA values with A=255.
-  # The 127 for RGB values was used merely for visualizing the mask
-  
-  # Draw two rectangles to fill interior with opaqueness
-  drawing_layer.polygon([(radius,0),(width-radius,0),
-                        (width-radius,height),(radius,height)],
-                        fill=(127,0,127,255))
-  drawing_layer.polygon([(0,radius),(width,radius),
-                        (width,height-radius),(0,height-radius)],
-                        fill=(127,0,127,255))
+image = Image.open('OriginalImages/family_1.jpg')
+image.thumbnail((200, 200))
+image.save('OriginalImages/family_1.jpg')
 
-  #Draw four filled circles of opaqueness
-  drawing_layer.ellipse((0,0, 2*radius, 2*radius), 
-                        fill=(0,127,127,255)) #top left
-  drawing_layer.ellipse((width-2*radius, 0, width,2*radius), 
-                        fill=(0,127,127,255)) #top right
-  drawing_layer.ellipse((0,height-2*radius,  2*radius,height), 
-                        fill=(0,127,127,255)) #bottom left
-  drawing_layer.ellipse((width-2*radius, height-2*radius, width, height), 
-                        fill=(0,127,127,255)) #bottom right
-                        
-  # Uncomment the following line to show the mask
-  # plt.imshow(rounded_mask)
-  
-  # Make the new image, starting with all transparent
-  result = PIL.Image.new('RGBA', original_image.size, (0,0,0,0))
-  result.paste(original_image, (0,0), mask=rounded_mask)
-  return result
-    
-def get_images(directory=None):
-  """
-""" Returns PIL.Image objects for all the images in directory.
-  
-  If directory is not specified, uses current directory.
-  Returns a 2-tuple containing 
-  a list with a  PIL.Image object for each image file in root_directory, and
-  a list with a string filename for each image file in root_directory
-  """
-"""
-  if directory == None:
-    directory = os.getcwd() # Use working directory if unspecified
 
-  image_list = [] # Initialize aggregaotrs
-  file_list = []
-  
-  directory_list = os.listdir(directory) # Get list of files
-  for entry in directory_list:
-    absolute_filename = os.path.join(directory, entry)
-    try:
-      image = PIL.Image.open(absolute_filename)
-      file_list += [entry]
-      image_list += [image]
-    except IOError:
-      pass # do nothing with errors tying to open non-images
-  return image_list, file_list
 
-def round_corners_of_all_images(directory=None):
-  """
-""" Saves a modfied version of each image in directory.
-  
-  Uses current directory if no directory is specified. 
-  Places images in subdirectory 'modified', creating it if it does not exist.
-  New image files are of type PNG and have transparent rounded corners.
-  """
-"""
-  if directory == None:
-    directory = os.getcwd() # Use working directory if unspecified
-    directory+='/OriginalImages'   
-  # Create a new directory 'modified'
-  new_directory = os.path.join(directory, '../modified')
-  try:
-    os.mkdir(new_directory)
-  except OSError:
-    pass # if the directory already exists, proceed  
-  
-  # Load all the images
-  image_list, file_list = get_images(directory)  
+im1 = Image.open('OriginalImages/result1.jpg')
+im2 = Image.open('OriginalImages/family_1.jpg')
 
-  # Go through the images and save modified versions
-  for n in range(len(image_list)):
-    # Parse the filename
-    print(n)
-    filename, filetype = os.path.splitext(file_list[n])
-
-    # Round the corners with default percent of radius
-    curr_image = image_list[n]
-    new_image = round_corners_one_image(curr_image) 
-    
-    # Save the altered image, suing PNG to retain transparency
-    new_image_filename = os.path.join(new_directory, filename + '.png')
-    new_image.save(new_image_filename)    
-
-round_corners_of_all_images(directory=None)
-"""
-from PIL import Image, ImageDraw, ImageFilter
-
-im1 = Image.open('OriginalImages/sunset.jpg')
-im2 = Image.open('OriginalImages/familycircle.jpg')
-
-mask_im = Image.new("L", im2.size, 0)
 mask_im = Image.new("L", im2.size, 0)
 draw = ImageDraw.Draw(mask_im)
-draw.ellipse((60, 10, 550, 400), fill=255)
-mask_im.save('mask_circle.jpg', quality=95)
+draw.ellipse((600, 50, 800, 250), fill=255)
+mask_im.save('OriginalImages/circle.jpg', quality=95)
 
 mask_im_blur = mask_im.filter(ImageFilter.GaussianBlur(10))
-mask_im_blur.save('mask_circle_blur.jpg', quality=95)
+mask_im_blur.save('circle_blur.jpg', quality=95)
 
 back_im = im1.copy()
 back_im.paste(im2, (0, 0), mask_im_blur)
-back_im.save('result1.jpg', quality=95)
+back_im.save('OriginalImages/result2.jpg', quality=95)
+'''
+JDoe_JSmith_1_4_2: Read and show an image.
+'''
+import matplotlib.pyplot as plt 
+import os.path
+import numpy as np      # 'as' lets us use standard abbreviations
+
+'''Read the image data'''
+# Get the directory of this python script
+directory = os.path.dirname(os.path.abspath(__file__)) 
+# Build an absolute filename from directory + filename
+filename = os.path.join(directory, 'result2.jpg')
+# Read the image data into an array
+img = plt.imread(filename)
+
+'''Show the image data'''
+# Create figure with 1 subplot
+fig, ax = plt.subplots(1, 1)
+# Show the image data in a subplot
+ax.imshow(img, interpolation='none')
+# Show the figure on the screen
+fig.show()
+fig.savefig('result2graph.jpg')
 
 
